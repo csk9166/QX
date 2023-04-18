754d948f3b1f188fca1d0779c9f47942817afc11
csk9166 <105415090+csk9166@users.noreply.github.com>
2023-04-19 05:10:10 +0800

    Update Weifeng.js

--- Weifeng.js
+++ Weifeng.js
@@ -1,4 +1,14 @@
+
 [rewrite_local]
 
+^https?:\/\/api\.wfdata\.club\/v1\/user\/homePageInfo.*? url script-response-body weifen.js
 
-^https?:\/\/api\.wfdata\.club\/v1\/user\/homePageInfo.*? url script-response-body https://raw.githubusercontent.com/csk9166/QX/main/Weifeng.js
+
+[mitm]
+hostname = api.wfdata.club
+%¥
+*******************************/
+var body=$response.body;
+body = body.replace(/"isVip\":false/g,'"isVip":true');
+body = body.replace(/"vipExpiryDate\":""/g,'"vipExpiryDate":"9999/09/09"');
+$done(body);
