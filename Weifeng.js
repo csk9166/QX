c416e23c033767b8c93084997f9a806d7f6f9498
csk9166 <105415090+csk9166@users.noreply.github.com>
2023-04-19 05:20:02 +0800

    Update Weifeng.js

--- Weifeng.js
+++ Weifeng.js
@@ -1,5 +1,5 @@
 [rewrite_local]
-^https?:\/\/api\.wfdata\.club\/v1\/user\/homePageInfo.*? url script-response-body weifeng.js
+^https?:\/\/api\.wfdata\.club\/v1\/user\/homePageInfo.*? url script-response-body https://raw.githubusercontent.com/csk9166/QX/main/Weifeng.js
 
 
 [mitm]
