4c5b34156b132aed8d69e065647ffd9037c21ace
csk9166 <105415090+csk9166@users.noreply.github.com>
2023-04-30 02:49:10 +0800

    Update and rename WS.js to WSGJ.js

--- WSGJ.js
+++ WSGJ.js
@@ -6,7 +6,7 @@
 *******************************
 
 [rewrite_local]
-^https?:\/\/api-v3\.speedtest\.cn\/user\/info.*? url script-response-body https://raw.githubusercontent.com/csk9166/QX/main/WS.js
+^https?:\/\/api-v3\.speedtest\.cn\/user\/info.*? url script-response-body WSGJ.js
 
 
 [mitm]
@@ -15,4 +15,5 @@ hostname = api-v3.speedtest.cn
 *******************************/
 var body=$response.body;
 body = body.replace(/isVip\":0/g,'isVip":1');
+body = body.replace(/vipExpire\":null/g,'vipExpire":"9999-06-20 04:01:19"');
 $done(body);
