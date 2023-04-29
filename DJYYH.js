191f0ab4efbeca5c5c253b37500871d2c74c3b37
csk9166 <105415090+csk9166@users.noreply.github.com>
2023-04-29 20:30:57 +0800

    Update DJYYH.js

--- DJYYH.js
+++ DJYYH.js
@@ -8,14 +8,4 @@
 *******************************
 %¥
 [rewrite_local]
-^https?:\/\/apphy2\.djyule\.com\/userLogin_2022md5\.asp.*? url script-response-body https://raw.githubusercontent.com/csk9166/QX/main/DJYYH
-
-
-[mitm]
-hostname = apphy2.djyule.com
-%¥
-*******************************/
-var body=$response.body;
-body = body.replace(/<HYVIP>0</HYVIP><VIPdate></VIPdate>/g,'<HYVIP>1</HYVIP><VIPdate>2099/09/09</VIPdate>');
-body = body.replace(/VIPgrade>0</VIPgrade>/g,'VIPgrade>1</VIPgrade>');
-$done(body);
+^https?:\/\/apphy2\.djyule\.com\/userLogin_2022md5\.asp.*? url script-response-body https://raw.githubusercontent.com/csk9166/QX/main/DJYYH.js
