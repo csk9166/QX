07a7ec9a3a05baba1962ebc4641319ec049ce48f
csk9166 <105415090+csk9166@users.noreply.github.com>
2023-05-14 04:34:47 +0800

    更新 DJYYH.js

--- DJyyh.js
+++ DJyyh.js
@@ -1,17 +1,19 @@
 /******************************
-
-脚本名称:DJ音乐盒
-
-
+⚠️如果放远程，请把DJ.js替换成运程链接⚠️
+  🧚🏻‍♂️作者：🍡魔法师、木木🍡
+  wx交流群：1077223830
+🫧脚本名称:DJ音乐盒
+🫧建议配合working copy一起食用
+✈️working copy下载地址https://apps.apple.com/app/id896694807✈️
 *******************************
-
+%¥
 [rewrite_local]
-^https?:\/\/apphy2\.djyule\.com\/userLogin_2022md5\.asp.*? url script-response-body https://github.com/csk9166/QX/blob/main/DJyyh.js
+^https?:\/\/apphy2\.djyule\.com\/userLogin_2022md5\.asp.*? url script-response-body DJ.js
 
 
 [mitm]
 hostname = apphy2.djyule.com
-
+%¥
 *******************************/
 var body=$response.body;
 body = body.replace(/<VIPdate></VIPdate>/g,'<VIPdate>2999/09/09</VIPdate>');
