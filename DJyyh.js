a5cb35b45d5ce504886fa8841c98512cab841d8a
csk9166 <105415090+csk9166@users.noreply.github.com>
2023-05-14 04:47:29 +0800

    Update DJyyh.js

--- DJyyh.js
+++ DJyyh.js
@@ -8,8 +8,7 @@
 *******************************
 %¥
 [rewrite_local]
-^https?:\/\/apphy2\.djyule\.com\/userLogin_2022md5\.asp.*? url script-response-body https://github.com/csk9166/QX/blob/main/DJyyh.js
-
+^https?:\/\/apphy2\.djyule\.com\/userLogin_2022md5\.asp.*? url script-response-body https://raw.githubusercontent.com/csk9166/QX/main/DJyyh.js
 
 [mitm]
 hostname = apphy2.djyule.com
