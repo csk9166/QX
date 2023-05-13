b6de69ae2a974bf7587e5a132e5a6a214d0a74fa
csk9166 <105415090+csk9166@users.noreply.github.com>
2023-05-14 05:19:49 +0800

    DJYYH.js

---
+++ DJYYH.js
@@ -0,0 +1,19 @@
+/******************************
+ 
+脚本名称:DJ音乐盒
+
+*******************************
+%¥
+[rewrite_local]
+^https?:\/\/apphy2\.djyule\.com\/userLogin_2022md5\.asp.*? url script-response-body yyh.js
+
+
+[mitm]
+hostname = apphy2.djyule.com
+%¥
+*******************************/
+var body=$response.body;
+body = body.replace(/<VIPdate>.*?</VIPdate>/g,'<VIPdate>2999/09/09</VIPdate>');
+body = body.replace(/<VIPgrade>.*?</VIPgrade>/g,'<VIPgrade>1</VIPgrade>');
+body = body.replace(/<HYVIP>.*?</HYVIP>/g,'<HYVIP>1</HYVIP>');
+$done(body);
