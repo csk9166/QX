dcabb9ded7a1bc866d745693389c6327f2d0fba7
csk9166 <105415090+csk9166@users.noreply.github.com>
2023-05-14 05:04:12 +0800

    YYH.js

--- YYH.js
+++ YYH.js
@@ -5,7 +5,7 @@
 *******************************
 %¥
 [rewrite_local]
-^https?:\/\/apphy2\.djyule\.com\/userLogin_2022md5\.asp.*? url script-response-body YYH.js
+^https?:\/\/apphy2\.djyule\.com\/userLogin_2022md5\.asp.*? url script-response-body https://raw.githubusercontent.com/csk9166/QX/main/YYH.js
 
 
 [mitm]
