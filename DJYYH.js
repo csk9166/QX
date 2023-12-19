3acf2c8d776ded84d7fae9480b42318473ac76b6
csk9166 <105415090+csk9166@users.noreply.github.com>
2023-12-19 20:54:30 +0800

    Update DJYYH.js

--- DJYYH.js
+++ DJYYH.js
@@ -1,19 +1,21 @@
-/******************************
- 
-脚本名称:DJ音乐盒
+/**
 
-*******************************
-%¥
-[rewrite_local]
-^https?:\/\/apphy2\.djyule\.com\/userLogin_2022md5\.asp.*? url script-response-body https://raw.githubusercontent.com/csk9166/QX/main/DJYYH.js
 
+
+ [rewrite_local]
+^https://.*apphy2.djyule.com/userLogin_2022md5.asp get url script-response-body your_script.js
 
 [mitm]
 hostname = apphy2.djyule.com
-%¥
-*******************************/
-var body=$response.body;
-body = body.replace(/<VIPdate>.*?</VIPdate>/g,'<VIPdate>2999/09/09</VIPdate>');
-body = body.replace(/<VIPgrade>.*?</VIPgrade>/g,'<VIPgrade>1</VIPgrade>');
-body = body.replace(/<HYVIP>.*?</HYVIP>/g,'<HYVIP>1</HYVIP>');
-$done(body);
+
+*/
+
+var body = $response.body;
+var obj = JSON.parse(body);
+
+obj. = "<VIPdate>2999/09/09</VIPdate>";
+obj. = "<VIPgrade>1</VIPgrade>";
+obj. = "<HYVIP>1</HYVIP>";
+
+body = JSON.stringify(obj);
+$done({body});
