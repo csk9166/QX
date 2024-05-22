1f8c8da0d0aa5cf88cf5ff1b0d7ffc8357705a63
csk9166 <105415090+csk9166@users.noreply.github.com>
2024-05-23 01:13:38 +0800

    DJyinyuehe.js

--- DJyinyuehe.js
+++ DJyinyuehe.js
@@ -1,19 +1,18 @@
+```javascript
 /******************************
- 
-脚本名称:DJ音乐盒
-
+脚本名称: DJ音乐盒
 *******************************
 %¥
 [rewrite_local]
 ^https?:\/\/apphy2\.djyule\.com\/userLogin_2022md5\.asp.*? url script-response-body https://raw.githubusercontent.com/csk9166/QX/main/DJyinyuehe.js
 
-
 [mitm]
 hostname = apphy2.djyule.com
 %¥
 *******************************/
-var body=$response.body;
-body = body.replace(/<VIPdate>.*?<\/VIPdate>/g,'<VIPdate>2999/09/09</VIPdate>');
-body = body.replace(/<VIPgrade>.*?<\/VIPgrade>/g,'<VIPgrade>1</VIPgrade>');
-body = body.replace(/<HYVIP>.*?<\/HYVIP>/g,'<HYVIP>1</HYVIP>');
+var body = $response.body;
+body = body.replace(/<VIPdate>.*?<\/VIPdate>/g, '<VIPdate>2999/09/09</VIPdate>');
+body = body.replace(/<VIPgrade>.*?<\/VIPgrade>/g, '<VIPgrade>1</VIPgrade>');
+body = body.replace(/<HYVIP>.*?<\/HYVIP>/g, '<HYVIP>1</HYVIP>');
 $done(body);
+```
