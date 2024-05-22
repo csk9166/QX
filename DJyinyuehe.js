c9a81aa2d3a02da9e3d911e2abf01c6a43964fcc
csk9166 <105415090+csk9166@users.noreply.github.com>
2024-05-23 01:15:37 +0800

    DJyinyuehe.js

--- DJyinyuehe.js
+++ DJyinyuehe.js
@@ -1,4 +1,4 @@
-```javascript
+javascript
 /******************************
 脚本名称: DJ音乐盒
 *******************************
@@ -15,4 +15,3 @@ body = body.replace(/<VIPdate>.*?<\/VIPdate>/g, '<VIPdate>2999/09/09</VIPdate>')
 body = body.replace(/<VIPgrade>.*?<\/VIPgrade>/g, '<VIPgrade>1</VIPgrade>');
 body = body.replace(/<HYVIP>.*?<\/HYVIP>/g, '<HYVIP>1</HYVIP>');
 $done(body);
-```
