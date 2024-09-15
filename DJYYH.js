ce5a05c96e1bc9f59af30b4c50422fe9db39df62
csk9166 <105415090+csk9166@users.noreply.github.com>
2024-09-15 11:07:54 +0800

    Update and rename DJyinyuehe.js to DJYYH.js

--- DJYYH.js
+++ DJYYH.js
@@ -3,7 +3,7 @@
 *******************************
 %¥
 [rewrite_local]
-^https?:\/\/apphy2\.djyule\.com\/userLogin_2022md5\.asp.*? url script-response-body https://raw.githubusercontent.com/csk9166/QX/main/DJyinyuehe.js
+^https?:\/\/apphy2\.djyule\.com\/userLogin_2022md5\.asp.*? url script-response-body https://raw.githubusercontent.com/csk9166/QX/main/DJYYH.js
 
 [mitm]
 hostname = apphy2.djyule.com
