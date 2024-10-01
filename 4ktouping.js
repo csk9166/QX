743ff314e22b55be07239a5dda289178e74ad49d
csk9166 <105415090+csk9166@users.noreply.github.com>
2024-10-01 17:49:23 +0800

    Update 4ktouping.js

--- 4ktouping.js
+++ 4ktouping.js
@@ -3,4 +3,4 @@
 http://111.229.140.167:8762/apptov5/v1/vod/getVod url script-response-body https://raw.githubusercontent.com/Alex0510/Eric/refs/heads/master/surge/Script/wm4k.js
 
 #投屏
-https?:\/\/omts\.tc\.qq\.com\/[^\s]+\.m3u8(\?[^\s]*)? url script-request-header https://raw.githubusercontent.com/Yu9191/Rewrite/main/lsp/wm4k.j
+https?:\/\/omts\.tc\.qq\.com\/[^\s]+\.m3u8(\?[^\s]*)? url script-request-header https://raw.githubusercontent.com/csk9166/XX/refs/heads/main/wm4k.js
